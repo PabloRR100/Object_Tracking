c2ca729d3709890ecf0fe971d095df18055b81db
PabloRR100 <pabloruizruiz10@gmail.com>
2018-09-04 17:07:46 -0400

    implementation of centroid tracker on webcam

--- 1 - Simple Object Tracking/main.py
+++ 1 - Simple Object Tracking/main.py
@@ -30,12 +30,77 @@ parser.add_argument('-c', '--confidence', typle=float, default=0.5,
 args = vars(parser.parse_args())
 
 
-# Centroid Tracker
+# Instantiate Centroid Tracker
 
 tracker = CentroidTracker()
 H, W = None, None
+rects = []
 
 # Pre-trained Deep Learning Detector
+
 net = cv2.dnn.readNetFromCaffe(args['prototxt'], args['model'])
 # net = cv2.dnn.readNetFromCaffe('deploy.prototxt', 'res10_300x300_ssd_iter_140000.caffemodel')
 
+
+# VIDEO CAPTURE
+
+video = VideoStream(src=0).start()
+time.sleep(2.0)
+
+while True:
+    
+    # 1 - Start video
+    frame = video.read()
+    frame = imutils.resize(frame, width=400)
+    
+    # 1.2 - Grap frame dimensions
+    if W is None or H is None: (H,W) = frame.shape[:2]
+
+    # 1.3 - Preprocessing the image - create a blob
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (W,H), (104., 177., 123.))
+    
+    # 1.4 - Forward pass on the model  
+    net.setInput(blob)
+    detections = net.forward()
+    
+    # 2 - Loop over the detections
+    
+    for i in range(detections.shape[2]):
+        
+        # 2.1 - Filter weak detections
+        if detections[0,0,i,2] > args['confidence']:
+            
+            # 2.2 - Compute (x,y) coordinates of the bounding box
+            box = detections[0,0,i,3:7] * np.array([W,H,W,H])
+            rects.append(box.astype('int'))
+            
+            # 2.3 - Draw the bounding box
+            (x0,y0,x1,y1) = box.astype('int')
+            cv2.rectangle(frame, (x0,y0), (x1,y1), (0,255,0), 2)
+            
+            # 3 - Pass the detections to our centroid tracker
+            
+            objects = tracker.update(rects)
+            
+            # 3.1 - Loop over the tacked objects
+            for objectID, centroid in objects.items():
+                
+                # 3.2 - Draw the ID and the centroid point
+                point_loc = tuple(centroid[0], centroid[1])
+                cv2.circle(frame, point_loc, 4, (0,255,0), -1)
+                
+                text = 'ID {}'.format(objectID)
+                text_loc = tuple(centroid[0] - 10, centroid[1] - 10)
+                cv2.putText(frame, text, text_loc, 
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 2)
+                
+            # 4 - Show output frame and wait to quit
+            cv2.imshow('Frame', frame)
+            key = cv2.waitKey(1) & 0xFF            
+            if key == ord(q): break
+        
+cv2.destroyAllWindows()
+video.stop()
+                
+    
+    
